626595031d6559375f869473a532fb4cb9e0a046
Corpus <programmercorpusis1@gmail.com>
2025-04-11 09:38:59 +0300

    queue

--- messageWorkerHandler.js
+++ messageWorkerHandler.js
@@ -1,3 +1,6 @@
+let lastHashSent = Date.now();
+let hashGroup = '';
+
 function handleWorkerMessage(workerIndex, str) {
     console.log('СРАБОТАЛ ОБРАБОТЧИК СООБЩЕНИЙ ОТ ВОРКЕРА');
     str = str.replace(/'/g, "");
@@ -8,7 +11,7 @@ function handleWorkerMessage(workerIndex, str) {
     switch (request) {
         case 'SendHash':
             let hash = value;
-            socket.send('SendHash#' + minerInfo.difficulty + ':' + hash + ':' + minerInfo.blockX + ':' + minerInfo.blockY + ':' + minerInfo.isStealth + MESSAGE_SPLITTER);
+            sendHash('SendHash#' + minerInfo.difficulty + ':' + hash + ':' + minerInfo.blockX + ':' + minerInfo.blockY + ':' + minerInfo.isStealth + MESSAGE_SPLITTER);
         break;
         case 'GetBlockDifficulty':
             workers[workerIndex].postMessage('GetBlockDifficulty#' + minerInfo.difficulty);
@@ -16,5 +19,15 @@ function handleWorkerMessage(workerIndex, str) {
         case 'GetNewBlock':
             workers[workerIndex].postMessage('GetNewBlock#' + JSON.stringify(minerInfo.block) + '^' + workerIndex);
             break;
+    }
+}
+
+function sendHash(hashMessage) {
+    hashGroup += hashMessage;
+    
+    if ((Date.now() - lastHashSent) / 1000 > 5) {
+        socket.send(hashGroup);
+        hashGroup = '';
+        lastHashSent = Date.now();
     }
 }
